eea3c4b24dd1b0be464322f39f2616ca2731b6bf
thunderistrash <99608599+aayn-a@users.noreply.github.com>
2025-03-13 18:04:56 -0700

    first commit

---
+++ model.py
@@ -0,0 +1,35 @@
+from sklearn.utils import compute_class_weight
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv("spam_or_not_spam.csv")
+emails = df["email"].to_numpy().astype(str)
+vectorizer = keras.layers.TextVectorization(max_tokens = 15000, output_sequence_length=50, output_mode="int")
+vectorizer.adapt(emails)
+X = vectorizer(emails)
+X = X.numpy()
+y = df["label"].to_numpy().astype(int)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+model = keras.Sequential([
+    keras.layers.Input(shape=(50,)),
+    keras.layers.Embedding(15000, 32),
+    keras.layers.Bidirectional(keras.layers.LSTM(64, return_sequences=False)),  
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(64, activation="relu"),
+    keras.layers.Dense(32, activation="relu"),
+    keras.layers.Dense(1, activation="sigmoid"),
+])
+
+
+class_labels = np.unique(y)
+class_weights = compute_class_weight(class_weight="balanced", classes=class_labels, y=y)
+class_weight_dict = {i: class_weights[i] for i in range(len(class_weights))}
+optimizer = keras.optimizers.Adam(learning_rate = 0.001)
+model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"])
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=20)
