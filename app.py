a1dbf7c9bc57e0f1ee3ea384cd9545608d520843
thunderistrash <99608599+aayn-a@users.noreply.github.com>
2025-03-13 20:10:47 -0700

    Started Creating Flask, Index.html

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, jsonify
-from training import train
+from flask import Flask, jsonify, render_template
 import tensorflow as tf
 from tensorflow import keras
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
-@app.route('/train-model', methods=['GET'])
-def train_model():
-    train()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 
 @app.route("/detectSpam", methods=["GET"])
 def use_model():
     model = tf.keras.models.load_model('spam_detector_model.h5')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
