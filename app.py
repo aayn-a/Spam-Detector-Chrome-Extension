acab904546f463f4c9a688217f813c46695bd8b7
thunderistrash <99608599+aayn-a@users.noreply.github.com>
2025-03-16 11:16:07 -0700

    changed app.py

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ from flask_cors import CORS
 
 
 app = Flask(__name__, template_folder="templates")
-model = keras.models.load_model('spam_detector_model.h5')
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        model = keras.models.load_model('spam_detector_model.h5')
+    return model
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,7 +25,7 @@ def index():
 def use_model():
     input = request.json['text']
     input_vectorized = preprocessWText(input)
-    prediction = model.predict(input_vectorized)
+    prediction = get_model().predict(input_vectorized)
     print(prediction)
     print("Spam" if prediction > 0.5 else "Not Spam")
     return jsonify({"prediction": "Spam" if prediction[0] > 0.5 else "Not Spam"})
