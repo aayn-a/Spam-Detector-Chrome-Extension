7f41196daf0db75c216f463d51a9a6f8844001fe
thunderistrash <99608599+aayn-a@users.noreply.github.com>
2025-03-17 09:12:39 -0700

    some more changes

--- app.py
+++ app.py
@@ -12,16 +12,17 @@ from tensorflow import keras
 
 app = Flask(__name__, template_folder="templates")
 MODEL_PATH = "spam_detector_model.h5"
-model = keras.models.load_model(MODEL_PATH)
+model = None
 
 CORS(app)
 
 def get_model():
     global model
     if model is None:
-        model = keras.models.load_model('spam_detector_model.h5')
+        print("Loading model...")
+        model = keras.models.load_model(MODEL_PATH)
+        print("Model loaded.")
     return model
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -30,14 +31,20 @@ def index():
 
 @app.route("/detectSpam", methods=["POST"])
 def use_model():
-    input_text = request.json["text"]
-    print(f"Received input: {input_text}")  # Log the received input
-    input_vectorized = preprocessWText(input_text)
-    prediction = model.predict(input_vectorized)
-    print(f"Prediction: {prediction}")  # Log the prediction
-    result = "Spam" if prediction[0] > 0.5 else "Not Spam"
-    print(f"Result: {result}")  # Log the result
-    return jsonify({"prediction": result})
+    try:
+        input_text = request.json['text']
+        print(f"Received input: {input_text}")  # Log the received input
+        input_vectorized = preprocessWText(input_text)
+        model = get_model()
+        prediction = model.predict(input_vectorized)
+        print(f"Prediction: {prediction}")  # Log the prediction
+        result = "Spam" if prediction[0] > 0.5 else "Not Spam"
+        print(f"Result: {result}")  # Log the result
+        return jsonify({"prediction": result})
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     PORT = int(os.getenv("PORT", 8080))  # Ensure port is an integer
